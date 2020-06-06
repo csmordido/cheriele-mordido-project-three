60fd01e0d2d965730a3d29fa31fddc24b684bba8
Cheriele Mordido <eyelmordido@gmail.com>
2020-06-06 11:48:41 -0400

    updated function to be re-usable

--- script.js
+++ script.js
@@ -8,20 +8,33 @@ tidyDesk.countHiddenImages = function() {
   return hiddenImagesN;
 }
 
-
-
-
-
-
-
-
-
-
-tidyDesk.init = () => {
+tidyDesk.startGame = function() {
   $(".modal-box button").on("click", function() {
     $(".modal-box").fadeOut(500);
     tidyDesk.listItem.fadeIn(500);
   })
+}
+
+tidyDesk.foundAllImages = function(numberOfImagesToFind) {
+  const numberOfHiddenImages = tidyDesk.countHiddenImages();
+  if ( numberOfHiddenImages === numberOfImagesToFind - 1) {
+    $(".modal-box")
+      .html(`<p>Congratulations! Your desk is ready for another bootcamp project!</p>
+      <button type="button">Play again</button>`)
+      .fadeIn(500);
+  }
+}
+
+tidyDesk.resetGame = function() {
+  $(".modal-box button").on("click", function() {
+    $(".modal-box").fadeOut(500);
+    tidyDesk.listItem.fadeIn(500);
+    $("img").fadeIn(500);
+  })
+}
+
+tidyDesk.init = () => {
+  tidyDesk.startGame();
 
   $("img").on("click", function() {
     const imgClass = $(this).attr("class");
@@ -30,23 +43,14 @@ tidyDesk.init = () => {
       tidyDesk.list
         .find("." + imgClass)
         .fadeOut(500);
-      const numberOfHiddenImages = tidyDesk.countHiddenImages();
-      if ( numberOfHiddenImages === 4) {
-        $(".modal-box")
-          .html(`<p>Congratulations! Your desk is ready for another bootcamp project!</p>
-          <button type="button">Play again</button>`)
-          .fadeIn(500);
-      }
-      $(".modal-box button").on("click", function() {
-        $(".modal-box").fadeOut(500);
-        tidyDesk.listItem.fadeIn(500);
-        $("img").fadeIn(500);
-      })
+
+      tidyDesk.foundAllImages(5);
     }
-    
+
+    tidyDesk.resetGame();
+
   })
 }
-
 
 $(function() {
   tidyDesk.init();

9fe411b47706ff0c599b4e946cb4ab4e0cbfb513
Cheriele Mordido <eyelmordido@gmail.com>
2020-06-07 14:29:56 -0400

    added timer

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const tidyDesk= {};
 
+tidyDesk.modalBox = $(".modal-box");
+tidyDesk.timerParagraph = $(".timer p");
+tidyDesk.timerText = $(".timer p span");
+tidyDesk.timerSeconds = $(".timer p span").text();
 tidyDesk.list = $(".images-list ul");
 tidyDesk.listItem = $(".images-list ul li");
 
@@ -10,15 +14,16 @@ tidyDesk.countHiddenImages = function() {
 
 tidyDesk.startGame = function() {
   $(".modal-box button").on("click", function() {
-    $(".modal-box").fadeOut(500);
-    tidyDesk.listItem.animate({opacity:1});
+    tidyDesk.modalBox.fadeOut(500);
+    tidyDesk.listItem.fadeTo(500, 1);
+    tidyDesk.timer(tidyDesk.timerSeconds);
   })
 }
 
 tidyDesk.foundAllImages = function(numberOfImagesToFind) {
   const numberOfHiddenImages = tidyDesk.countHiddenImages();
   if ( numberOfHiddenImages === numberOfImagesToFind - 1) {
-    $(".modal-box")
+    tidyDesk.modalBox
       .html(`<p>Congratulations! Your desk is ready for another bootcamp project!</p>
       <button type="button">Play again</button>`)
       .fadeIn(500);
@@ -27,10 +32,35 @@ tidyDesk.foundAllImages = function(numberOfImagesToFind) {
 
 tidyDesk.resetGame = function() {
   $(".modal-box button").on("click", function() {
-    $(".modal-box").fadeOut(500);
-    tidyDesk.listItem.animate({opacity:1});;
+    tidyDesk.modalBox.fadeOut(500);
+    tidyDesk.listItem.fadeTo(500, 1);
     $("img").fadeIn(500);
+    tidyDesk.timer(tidyDesk.timerSeconds);
   })
+}
+
+tidyDesk.timer = function(seconds) {
+  let counter = seconds;
+  let timer = setInterval(function() {
+    tidyDesk.timerParagraph.fadeTo("fast", 1);
+    tidyDesk.timerText.text(counter);
+    counter--;
+    numberOfHiddenImages = tidyDesk.countHiddenImages();
+    if (numberOfHiddenImages === 5) {
+      clearInterval(timer);
+      tidyDesk.timerParagraph.fadeTo("fast", 0.5);
+      tidyDesk.timerText.text(seconds);
+    } else if (counter === 0) {
+      clearInterval(timer);
+      tidyDesk.timerParagraph.fadeTo("fast", 0.5);
+      tidyDesk.timerText.text(seconds);
+      tidyDesk.modalBox
+        .html(`<p>Time's up!</p>
+        <button type="button">Play again</button>`)
+        .fadeIn(500);
+      tidyDesk.resetGame();
+    }
+  }, 1000);
 }
 
 tidyDesk.init = () => {
@@ -42,7 +72,7 @@ tidyDesk.init = () => {
       $(this).fadeOut(500);
       tidyDesk.list
         .find("." + imgClass)
-        .animate({opacity:0});
+        .fadeTo(500, 0);
       tidyDesk.foundAllImages(5);
     }
 
